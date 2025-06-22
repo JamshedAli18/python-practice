f547d34dc224809d2ce6586373515236a3fe806a
Jamshed Ali <116933185+JamshedAli18@users.noreply.github.com>
2025-06-22 09:58:02 +0500

    Create  Vehicle System – Abstraction using Abstract Base Class.py

---
+++ Vehicle System – Abstraction using Abstract Base Class.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+    @abstractmethod
+    def start(self):
+        pass
+
+class Car(Vehicle):
+    def start(self):
+        print("Car started")
+
+class Bike(Vehicle):
+    def start(self):
+        print("Bike started")
+
+# Example
+v1 = Car()
+v2 = Bike()
+v1.start()
+v2.start()
