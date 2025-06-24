63f2010f6340d3ee0971fdc6e20710a6268d53c3
Jamshed Ali <116933185+JamshedAli18@users.noreply.github.com>
2025-06-24 21:28:29 +0500

    Update inheritance.py

--- inheritance.py
+++ inheritance.py
@@ -1,1 +1,13 @@
+class Animal:
+    def speak(self):
+        print("Animal speaks")
+
+class Dog(Animal):
+    def bark(self):
+        print("Dog barks")
+
+# Example
+d = Dog()
+d.speak()  # from parent
+d.bark()   # from child
 
